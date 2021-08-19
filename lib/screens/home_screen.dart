c0a4616c689765f0fdc6218c17030a5a00c4a8bf
Nonari Sanda <mkg_sandy@icloud.com>
2021-08-19 22:28:27 +0900

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../widgets/sample_message.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SampleMessage(),
+    );
+  }
+}
